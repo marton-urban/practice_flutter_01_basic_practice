29d58536c8bfb95459adae66dca990a42a321b06
urban.marton <urban.marton@gmail.com>
2022-03-21 23:13:05 +0100

    simpler interface

--- lib/constraints.dart
+++ lib/constraints.dart
@@ -14,210 +14,49 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FlutterLayoutArticle([
-      Example14(),
-    ]);
-  }
-}
-
-//////////////////////////////////////////////////
-
-abstract class Example extends StatelessWidget {
-  const Example({Key? key}) : super(key: key);
-
-  String get code;
-
-  String get explanation;
-}
-
-//////////////////////////////////////////////////
-
-class FlutterLayoutArticle extends StatefulWidget {
-  const FlutterLayoutArticle(
-    this.examples, {
-    Key? key,
-  }) : super(key: key);
-
-  final List<Example> examples;
-
-  @override
-  _FlutterLayoutArticleState createState() => _FlutterLayoutArticleState();
-}
-
-//////////////////////////////////////////////////
-
-class _FlutterLayoutArticleState extends State<FlutterLayoutArticle> {
-  late int count;
-  late Widget example;
-  late String code;
-  late String explanation;
-
-  @override
-  void initState() {
-    count = 1;
-    code = const Example14().code;
-    explanation = const Example14().explanation;
-
-    super.initState();
-  }
-
-  @override
-  void didUpdateWidget(FlutterLayoutArticle oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    var example = widget.examples[count - 1];
-    code = example.code;
-    explanation = example.explanation;
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Layout Article',
-      home: SafeArea(
-        child: Material(
-          color: Colors.black,
-          child: FittedBox(
-            child: Container(
-              width: 400,
-              height: 670,
-              color: const Color(0xFFCCCCCC),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: ConstrainedBox(
-                          constraints: const BoxConstraints.tightFor(
-                              width: double.infinity, height: double.infinity),
-                          child: widget.examples[count - 1])),
-                  Container(
-                    height: 50,
-                    width: double.infinity,
-                    color: Colors.black,
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          for (int i = 0; i < widget.examples.length; i++)
-                            Container(
-                              width: 58,
-                              padding:
-                                  const EdgeInsets.only(left: 4.0, right: 4.0),
-                              child: button(i + 1),
-                            ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Scrollbar(
-                      child: SingleChildScrollView(
-                        key: ValueKey(count),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            children: [
-                              Center(child: Text(code)),
-                              const SizedBox(height: 15),
-                              Text(
-                                explanation,
-                                style: TextStyle(
-                                    color: Colors.blue[900],
-                                    fontStyle: FontStyle.italic),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    height: 273,
-                    color: Colors.grey[50],
-                  ),
-                ],
+      title: 'Flutter Practice',
+      home: Material(
+        child: Center(
+          child: Container(width: 406, height: 353, decoration: BoxDecoration(border: Border.all(width: 3)),
+            child:
+
+Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        /*1*/
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /*2*/
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(
+                color: Colors.grey[500],
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  Widget button(int exampleNumber) {
-    return Button(
-      key: ValueKey('button$exampleNumber'),
-      isSelected: count == exampleNumber,
-      exampleNumber: exampleNumber,
-      onPressed: () {
-        showExample(
-          exampleNumber,
-          widget.examples[exampleNumber - 1].code,
-          widget.examples[exampleNumber - 1].explanation,
-        );
-      },
-    );
-  }
-
-  void showExample(int exampleNumber, String code, String explanation) {
-    setState(() {
-      count = exampleNumber;
-      this.code = code;
-      this.explanation = explanation;
-    });
-  }
-}
-
-//////////////////////////////////////////////////
-
-class Button extends StatelessWidget {
-  final bool isSelected;
-  final int exampleNumber;
-  final VoidCallback onPressed;
-
-  const Button({
-    required Key key,
-    required this.isSelected,
-    required this.exampleNumber,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      style: TextButton.styleFrom(
-        primary: Colors.white,
-        backgroundColor: isSelected ? Colors.grey : Colors.grey[800],
+      /*3*/
+      Icon(
+        Icons.star,
+        color: Colors.red[500],
       ),
-      child: Text(exampleNumber.toString()),
-      onPressed: () {
-        Scrollable.ensureVisible(
-          context,
-          duration: const Duration(milliseconds: 350),
-          curve: Curves.easeOut,
-          alignment: 0.5,
-        );
-        onPressed();
-      },
-    );
-  }
-}
-//////////////////////////////////////////////////
+      const Text('41'),
+    ],
+  ))
 
-class Example14 extends Example {
-  const Example14({Key? key}) : super(key: key);
-
-  @override
-  final code = '';
-  @override
-  final String explanation = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return UnconstrainedBox(
-      child: LimitedBox(
-          maxWidth: 150,
-          maxHeight: 200,
-          child: Container(color: red, width: 50, height: 150)),
-    );
-  }
-}
+))));}}
