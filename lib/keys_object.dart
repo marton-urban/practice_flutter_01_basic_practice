eab24b06b17b96f150cff1a36cd1d55446842f57
Marton Urban <urban.marton@gmail.com>
2023-05-28 11:30:15 +0200

    add new file

---
+++ lib/keys_object.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(SameValuesPage());
+}
+
+class SameValuesPage extends StatefulWidget {
+  @override
+  createState() => _SameValuesPageState();
+}
+
+class _SameValuesPageState extends State<SameValuesPage> {
+  late List<User> users;
+
+  @override
+  void initState() {
+    super.initState();
+// const will NOT work for this example; only max one of them can be const here
+    final peter1 = User(
+      name: 'Peter Drucker',
+      country: 'USA',
+    );
+
+    final peter2 = User(
+      name: 'Peter Drucker',
+      country: 'USA',
+    );
+
+    users = [
+      peter1,
+      peter2,
+      const User(
+        name: 'Sarah Abs',
+        country: 'England',
+      ),
+      const User(
+        name: 'James High',
+        country: 'India',
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Object Key'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              children: users
+                  .map<Widget>((user) => UserWidget(
+                        key: ObjectKey(user),
+                        user: user,
+                      ))
+                  .toList(),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: swapTiles,
+            child: const Icon(Icons.swap_horiz, size: 32),
+          ),
+        ),
+      );
+
+  void swapTiles() => setState(() {
+        final user = users.removeAt(0);
+        users.insert(1, user);
+      });
+}
+
+class UserWidget extends StatefulWidget {
+  final User user;
+
+  const UserWidget({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  createState() => _UserWidgetState();
+}
+
+class _UserWidgetState extends State<UserWidget> {
+  late Color color;
+
+  @override
+  void initState() {
+    super.initState();
+
+    color = generateColor();
+  }
+
+  Color generateColor() {
+    final random = Random();
+
+    return Color.fromARGB(
+      255,
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: color,
+        child: ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          title: Text(widget.user.name),
+          subtitle: Text(widget.user.country),
+          trailing: Text('HashCode: ${widget.user.hashCode}'),
+        ),
+      );
+}
+
+class User {
+  final String name;
+  final String country;
+
+  const User({
+    required this.name,
+    required this.country,
+  });
+}
