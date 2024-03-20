d18894ff6fac65aa46f23a5806751ab2e9306bbc
Marton Urban <urban.marton@gmail.com>
2024-03-20 19:33:01 +0100

    more features for FutureBuilder

--- lib/future_builder.dart
+++ lib/future_builder.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainPage());
@@ -12,9 +15,23 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  Future<int> getData() async {
-    await Future.delayed(const Duration(seconds: 4));
-    return 7;
+  late Future<int?> dataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    dataFuture = getData();
+  }
+
+  Future<int?> getData() async {
+    final result = await http
+        .get(Uri.parse('https://www.randomnumberapi.com/api/v1.0/random'));
+    await Future.delayed(const Duration(seconds: 3));
+    final body = json.decode(result.body);
+    int randomNumber = (body as List).first;
+    return randomNumber;
+    // return null;
+    // throw UnimplementedError();
   }
 
   @override
@@ -23,15 +40,26 @@ class _MainPageState extends State<MainPage> {
         appBar: AppBar(
           title: const Text('Future Builder'),
         ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.refresh),
+            onPressed: () => setState(() {
+                  dataFuture = getData();
+                })),
         body: Center(
             child: FutureBuilder<int?>(
-                future: getData(),
+                future: dataFuture,
+                initialData: 5, // never shown in this example
                 builder: (context, snapshot) {
-                  if (snapshot.hasData) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    final error = snapshot.error;
+                    return Text('🥺$error');
+                  } else if (snapshot.hasData) {
                     int? data = snapshot.data;
                     return Text('✅ $data');
                   } else {
-                    return const CircularProgressIndicator();
+                    return const Text('No data');
                   }
                 })),
       ));
